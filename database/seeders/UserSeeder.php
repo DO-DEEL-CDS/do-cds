d269b4de08f21c7a878aa5ec3539bfed5cd0350f
DouglasOkolaa <okolaadouglas@gmail.com>
2021-10-17 18:41:52 +0100

    chore: user database seeder
    Signed-off-by: DouglasOkolaa <okolaadouglas@gmail.com>

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class ProfileSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,6 +14,8 @@ class ProfileSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::factory(10)
+            ->hasProfile()
+            ->create();
     }
 }
