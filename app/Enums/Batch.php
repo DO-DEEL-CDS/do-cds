b7f28ced2296d22dc7f5c5da4deb172018657042
DouglasOkolaa <okolaadouglas@gmail.com>
2021-11-03 22:46:34 +0100

    chore: Increase batch list

--- app/Enums/Batch.php
+++ app/Enums/Batch.php
@@ -6,13 +6,25 @@ use BenSampo\Enum\Enum;
 
 /**
  * @method static static BatchAStream1()
+ * @method static static BatchAStream2()
+ * @method static static BatchAStream3()
+ * @method static static BatchBStream1()
  * @method static static BatchBStream2()
+ * @method static static BatchBStream3()
+ * @method static static BatchCStream1()
+ * @method static static BatchCStream2()
  * @method static static BatchCStream3()
  */
 final class Batch extends Enum
 {
     public const BatchAStream1 = 'BatchAStream1';
+    public const BatchAStream2 = 'BatchAStream2';
+    public const BatchAStream3 = 'BatchAStream3';
+    public const BatchBStream1 = 'BatchBStream1';
     public const BatchBStream2 = 'BatchBStream2';
+    public const BatchBStream3 = 'BatchBStream3';
+    public const BatchCStream1 = 'BatchCStream1';
+    public const BatchCStream2 = 'BatchCStream2';
     public const BatchCStream3 = 'BatchCStream3';
 
 
